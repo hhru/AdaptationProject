fa2dc39f066776aec5b30b56c45e3b893fdbf4e4
Leonid Vinogradov <leonid.52669@gmail.com>
2018-03-26 20:52:20 +0300

    TASK-6 bugfix

--- backend/src/main/resources/db/migration/V4__Create_transition_table.sql
+++ backend/src/main/resources/db/migration/V4__Create_transition_table.sql
@@ -1,14 +1,18 @@
-CREATE TYPE workflow_step AS ENUM ('Add','TaskList', 'WelcomeMeeting', 'InterimMeeting', 'InterimMeetingResult', 'FinalMeeting', 'FinalMeetingResult', 'Questionnaire');
-
-CREATE TYPE workflow_step_status AS ENUM ('Done', 'Coming', 'Notcoming', 'Overdue', 'Ignore');
-
 CREATE TABLE transition
 (
   id SERIAL PRIMARY KEY,
   employee_id INT REFERENCES employee(id),
-  step workflow_step default 'Add' NOT NULL,
-  step_status workflow_step_status default 'Coming' NOT NULL,
+  step VARCHAR(30) NOT NULL UNIQUE,
+  step_status VARCHAR(20) NOT NULL,
   deadline_timestamp TIMESTAMP,
   comment text
 );
-insert into transition (employee_id, deadline_timestamp) VALUES (1, '2024-10-19 10:23:54');
+
+insert into transition (employee_id, step, step_status, deadline_timestamp) VALUES (1, 'ADD', 'COMING', '2024-10-19 10:23:54');
+insert into transition (employee_id, step, step_status, deadline_timestamp) VALUES (1, 'TASKLIST', 'NOTCOMING', '2025-10-19 10:23:54');
+insert into transition (employee_id, step, step_status, deadline_timestamp) VALUES (1, 'WELCOMEMEETING', 'NOTCOMING', '2026-10-19 10:23:54');
+insert into transition (employee_id, step, step_status, deadline_timestamp) VALUES (1, 'INTERIMMEETING', 'NOTCOMING', '2027-10-19 10:23:54');
+insert into transition (employee_id, step, step_status, deadline_timestamp) VALUES (1, 'INTERIMMEETINGRESULT', 'NOTCOMING', '2028-10-19 10:23:54');
+insert into transition (employee_id, step, step_status, deadline_timestamp) VALUES (1, 'FINALMEETING', 'NOTCOMING', '2029-10-19 10:23:54');
+insert into transition (employee_id, step, step_status, deadline_timestamp) VALUES (1, 'FINALMEETINGRESULT', 'NOTCOMING', '2030-10-19 10:23:54');
+insert into transition (employee_id, step, step_status, deadline_timestamp) VALUES (1, 'QUESTIONNAIRE', 'NOTCOMING', '2031-10-19 10:23:54');
