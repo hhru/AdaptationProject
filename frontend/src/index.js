952e7f8229a98c976b64692244fe723d3b626fa5
Vitaly Vasin <neural.null@gmail.com>
2018-03-28 20:54:01 +0300

    TASK-10 Code style fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,4 +14,6 @@ const render = Component =>
 render(App);
 
 // Webpack Hot Module Replacement API
-if (module.hot) module.hot.accept('./components/App', () => render(App));
+if (module.hot) {
+  module.hot.accept('./components/App', () => render(App));
+}
