0b5ea21461a0301f58cc2cebf41f8428a813a3a3
mvxio <me@mvx.io>
2018-04-04 17:35:42 +0300

    Remove example classes.

--- backend/src/test/java/ru/hh/school/adaptation/TestBase.java
+++ backend/src/test/java/ru/hh/school/adaptation/TestBase.java
@@ -3,17 +3,7 @@ package ru.hh.school.adaptation;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
-import ru.hh.school.adaptation.resources.ExampleResource;
-
-import javax.inject.Inject;
 
 @ContextConfiguration(classes = {TestConfig.class})
 public abstract class TestBase extends AbstractJUnit4SpringContextTests {
-
-  @Inject
-  ExampleResource resource;
-
-  protected ExampleResource getExampleResource() {
-    return resource;
-  }
 }
