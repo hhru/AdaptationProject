04ffabd1f6f378cdabae64fbff883aeffe26e3e2
Anna Lioznova <lanna2009@gmail.com>
2018-04-05 02:55:59 +0300

    employee page

--- frontend/src/components/EmployeePage.js
+++ frontend/src/components/EmployeePage.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import FaAdjust from 'react-icons/lib/fa/adjust';
-import FaCheckCircle from 'react-icons/lib/fa/check-circle';
-import FaCircleO from 'react-icons/lib/fa/circle-o';
-import FaClockO from 'react-icons/lib/fa/clock-o';
 import FaCircle from 'react-icons/lib/fa/circle';
+import FaAdjust from 'react-icons/lib/fa/adjust';
 import FaExclamationCircle from 'react-icons/lib/fa/exclamation-circle';
-import FaPlusCircle from 'react-icons/lib/fa/plus-circle';
-import FaQuestionCircle from 'react-icons/lib/fa/question-circle';
-import FaSmileO from 'react-icons/lib/fa/smile-o';
-import FaTimesCircle from 'react-icons/lib/fa/times-circle';
-import FaEdit from 'react-icons/lib/fa/edit';
+import FaCheckCircle from 'react-icons/lib/fa/check-circle';
 
 class EmployeePage extends React.Component {
   constructor(props) {
@@ -21,6 +14,7 @@ class EmployeePage extends React.Component {
       employeeId: this.props.location.search,
       data: {
         id: null,
+        currentWorkflowStep: '',
         self: {
           id: null,
           firstName: '',
@@ -53,23 +47,22 @@ class EmployeePage extends React.Component {
           email: '',
           inside: '',
         },
-        employmentTimestamp: null,
+        employmentDate: null,
         workflow: [
           {
             id: null,
             type: '',
             status: '',
-            deadlineTimestamp: null,
+            deadlineDate: null,
             comment: null,
+            overdue: false,
           },
         ],
       },
     };
-
-    this.handleGetEmployeeInfo = this.handleGetEmployeeInfo.bind(this);
   }
 
-  handleGetEmployeeInfo(response) {
+  componentDidMount() {
     let responseData = {
       id: 1,
       self: {
@@ -100,130 +93,111 @@ class EmployeePage extends React.Component {
         id: 1,
         firstName: 'Билл',
         lastName: 'Гейтс',
-        middleName: null,
+        middleName: 'В.',
         email: 'gates@microsoft.com',
         inside: 'gates',
       },
-      employmentTimestamp: '2018-03-31 08:53:46',
+      employmentDate: '2018-03-31',
+      currentWorkflowStep: 'TASK_LIST',
       workflow: [
         {
           id: 8,
           type: 'ADD',
-          status: 'DONE',
-          deadlineTimestamp: '2018-03-31',
+          status: 'COMPLETE',
+          deadlineDate: '2018-03-31',
           comment: null,
+          overdue: false,
         },
         {
           id: 7,
           type: 'TASK_LIST',
-          status: 'OVERDUE',
-          deadlineTimestamp: '2018-03-31',
+          status: 'COMPLETE',
+          deadlineDate: '2018-03-31',
           comment: null,
+          overdue: false,
         },
         {
           id: 6,
           type: 'WELCOME_MEETING',
-          status: 'CURRENT',
-          deadlineTimestamp: '2018-03-31',
+          status: 'IN_PROGRESS',
+          deadlineDate: '2018-03-31',
           comment: null,
+          overdue: false,
         },
         {
           id: 5,
           type: 'INTERIM_MEETING',
           status: 'NOT_DONE',
-          deadlineTimestamp: '2018-03-31',
+          deadlineDate: '2018-03-31',
           comment: null,
+          overdue: true,
         },
         {
           id: 4,
           type: 'INTERIM_MEETING_RESULT',
           status: 'NOT_DONE',
-          deadlineTimestamp: '2018-03-31',
+          deadlineDate: '2018-03-31',
           comment: null,
+          overdue: false,
         },
         {
           id: 3,
           type: 'FINAL_MEETING',
           status: 'NOT_DONE',
-          deadlineTimestamp: '2018-03-31',
+          deadlineDate: '2018-03-31',
           comment: null,
+          overdue: false,
         },
         {
           id: 2,
           type: 'FINAL_MEETING_RESULT',
           status: 'NOT_DONE',
-          deadlineTimestamp: '2018-03-31',
+          deadlineDate: '2018-03-31',
           comment: null,
+          overdue: false,
         },
         {
           id: 1,
           type: 'QUESTIONNAIRE',
           status: 'NOT_DONE',
-          deadlineTimestamp: '2018-03-31',
+          deadlineDate: '2018-03-31',
           comment: null,
+          overdue: false,
         },
       ],
     };
+    //$.get('/api/employee/' + sthis.state.employeeId);
     this.setState({
       data: responseData,
     });
-  }
-
-  componentDidMount() {
-    let response;
-    //        $.get('/api/employee/' + sthis.state.employeeId, this.handleGetEmployeeInfo);
-    this.handleGetEmployeeInfo(response);
   }
 
   render() {
     return (
       <div>
         <p>
-          {this.state.data.self.firstName +
-            ' ' +
-            this.state.data.self.middleName +
-            ' ' +
-            this.state.data.self.lastName}
+          {`${this.state.data.self.firstName} ${
+            this.state.data.self.middleName
+          } ${this.state.data.self.lastName}`}
         </p>
         <p>{this.state.data.self.email}</p>
         <p>
-          {'Начальник: ' +
-            this.state.data.chief.firstName +
-            ' ' +
-            this.state.data.chief.middleName +
-            ' ' +
-            this.state.data.chief.lastName}
+          {`Начальник: ${this.state.data.chief.firstName} ${
+            this.state.data.chief.middleName
+          } ${this.state.data.chief.lastName}`}
         </p>
         <p>
-          {'Ментор: ' +
-            this.state.data.mentor.firstName +
-            ' ' +
-            this.state.data.mentor.middleName +
-            ' ' +
-            this.state.data.mentor.lastName}
+          {`Ментор: ${this.state.data.mentor.firstName} ${
+            this.state.data.mentor.middleName
+          } ${this.state.data.mentor.lastName}`}
         </p>
         <p>
-          {'HR: ' +
-            this.state.data.hr.firstName +
-            ' ' +
-            this.state.data.hr.middleName +
-            ' ' +
-            this.state.data.hr.lastName}
+          {`HR: ${this.state.data.hr.firstName} ${
+            this.state.data.hr.middleName
+          } ${this.state.data.hr.lastName}`}
         </p>
-        <p>{'Дата выхода: ' + this.state.data.employmentTimestamp}</p>
+        <p>{`Дата выхода: ${this.state.data.employmentDate}`}</p>
         <Workflow data={this.state.data.workflow} />
-        <br />
-        <FaAdjust />
-        <FaCheckCircle />
-        <FaExclamationCircle />
-        <FaCircle />
-        <FaCircleO />
-        <FaClockO />
-        <FaPlusCircle />
-        <FaQuestionCircle />
-        <FaSmileO />
-        <FaTimesCircle />
-        <FaEdit />
       </div>
     );
   }
@@ -234,26 +208,26 @@ class Workflow extends React.Component {
     super(props);
 
     this.renderWorkflowStage = this.renderWorkflowStage.bind(this);
-    this.selectIcon = this.selectIcon.bind(this);
-  }
-
-  selectIcon(workflowStage) {
-    if (workflowStage.status == 'NOT_DONE') {
-      return <FaCircle size={50} color="grey" />;
-    } else if (workflowStage.status == 'CURRENT') {
-      return <FaAdjust size={50} color="yellow" />;
-    } else if (workflowStage.status == 'OVERDUE') {
-      return <FaExclamationCircle size={50} color="red" />;
-    } else if (workflowStage.status == 'DONE') {
-      return <FaCheckCircle size={50} color="green" />;
-    }
   }
 
   renderWorkflowStage(workflowStage) {
+    let iconMap = {
+      true: {
+        IN_PROGRESS: <FaExclamationCircle size={50} color="red" />,
+        COMPLETE: <FaExclamationCircle size={50} color="red" />,
+        NOT_DONE: <FaExclamationCircle size={50} color="red" />,
+      },
+      false: {
+        IN_PROGRESS: <FaAdjust size={50} color="yellow" />,
+        COMPLETE: <FaCheckCircle size={50} color="green" />,
+        NOT_DONE: <FaCircle size={50} color="grey" />,
+      },
+    };
+
     return (
       <div>
-        {workflowStage.deadlineTimestamp}
-        {this.selectIcon(workflowStage)}
+        {workflowStage.deadlineDate}
+        {iconMap[workflowStage['overdue']][workflowStage['status']]}
         {workflowStage.type}
         <br />
       </div>
