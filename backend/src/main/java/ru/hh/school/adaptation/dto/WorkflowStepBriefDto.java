c27ae94fcab361160297ddafe40d5a45fd6c10a3
desile <pl9lar@gmail.com>
2018-05-09 12:40:07 +0300

    TASK-51 Расширение DTO списка под роу эспандер

--- backend/src/main/java/ru/hh/school/adaptation/dto/WorkflowStepBriefDto.java
+++ backend/src/main/java/ru/hh/school/adaptation/dto/WorkflowStepBriefDto.java
@@ -1,5 +1,7 @@
 package ru.hh.school.adaptation.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import ru.hh.school.adaptation.AdaptationCommonConfig;
 import ru.hh.school.adaptation.entities.Transition;
 import ru.hh.school.adaptation.entities.WorkflowStepStatus;
 import ru.hh.school.adaptation.entities.WorkflowStepType;
@@ -14,12 +16,16 @@ public class WorkflowStepBriefDto {
 
   public WorkflowStepStatus status;
 
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = AdaptationCommonConfig.JSON_DATE_TIME_FORMAT)
+  public Date deadlineTimestamp;
+
   public boolean overdue;
 
   public WorkflowStepBriefDto(Transition transition) {
     id = transition.getId();
     type = transition.getStepType();
     status = transition.getStepStatus();
+    deadlineTimestamp = transition.getDeadlineTimestamp();
     overdue = transition.getDeadlineTimestamp() != null && (new Date()).after(transition.getDeadlineTimestamp());
   }
 
