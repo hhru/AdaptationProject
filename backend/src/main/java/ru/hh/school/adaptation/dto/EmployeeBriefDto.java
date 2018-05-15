fcae4669eee2765f12408b8b1a6423da8cb630d4
Paul Caser <pl9lar@gmail.com>
2018-05-15 22:58:42 +0300

    TASK-51 Расширение DTO списка под роу эспандер
    * TASK-51 Расширение DTO списка под роу эспандер
    
    * Trigger travis

--- backend/src/main/java/ru/hh/school/adaptation/dto/EmployeeBriefDto.java
+++ backend/src/main/java/ru/hh/school/adaptation/dto/EmployeeBriefDto.java
@@ -12,13 +12,13 @@ public class EmployeeBriefDto {
 
   public Integer id;
 
-  public String firstName;
+  public PersonalDto employee;
 
-  public String lastName;
+  public PersonalDto chief;
 
-  public String middleName;
+  public PersonalDto mentor;
 
-  public String hrName;
+  public PersonalDto hr;
 
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = AdaptationCommonConfig.JSON_DATE_FORMAT)
   public Date employmentDate;
@@ -27,14 +27,13 @@ public class EmployeeBriefDto {
 
   public EmployeeBriefDto(Employee employee) {
     id = employee.getId();
-    firstName = employee.getSelf().getFirstName();
-    lastName = employee.getSelf().getLastName();
-    middleName = employee.getSelf().getMiddleName();
+    this.employee = new PersonalDto(employee.getSelf());
+    chief = new PersonalDto(employee.getChief());
+    if (employee.getMentor() != null) {
+      mentor = new PersonalDto(employee.getMentor());
+    }
+    hr = new PersonalDto(employee.getHr());
     employmentDate = employee.getEmploymentDate();
-    hrName = String.format("%s %s",
-        employee.getHr().getSelf().getLastName(),
-        employee.getHr().getSelf().getFirstName()
-    );
     workflow = employee.getWorkflow().stream().map(WorkflowStepBriefDto::new).collect(Collectors.toList());
   }
 }
