e2eff0fcbe2d41eca8e2a19d581f612270bf124b
Leonid Vinogradov <leonid.52669@gmail.com>
2018-05-20 17:34:11 +0300

    TASK-58 set email agent to gmail

--- backend/src/main/java/ru/hh/school/adaptation/services/workflow/InterimMeetingResultStep.java
+++ backend/src/main/java/ru/hh/school/adaptation/services/workflow/InterimMeetingResultStep.java
@@ -3,9 +3,6 @@ package ru.hh.school.adaptation.services.workflow;
 import ru.hh.school.adaptation.entities.Employee;
 import ru.hh.school.adaptation.services.MailService;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class InterimMeetingResultStep {
   private MailService mailService;
 
@@ -14,10 +11,5 @@ public class InterimMeetingResultStep {
   }
 
   public void onInterimMeetingResult(Employee employee) {
-    Map<String, String> params = new HashMap<>();
-    params.put("{{userName}}", employee.getSelf().getFirstName() + " " + employee.getSelf().getLastName());
-    params.put("{{meetingType}}", "промежуточная");
-    params.put("{{url}}", "http://results.com");
-    mailService.sendMail(employee.getHr().getSelf().getEmail(), "meeting_results", params);
   }
 }
