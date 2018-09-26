6fefbaab71cabe2781ec1be321ae498d4c90ee0f
Vladimir Maximov <v.maksimov@hh.ru>
2018-09-26 08:32:42 +0300

    ADAPT-21 collapsing header

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,59 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { Row, Col, Button, Form } from 'reactstrap';
+import { Jumbotron } from 'reactstrap';
 
-const mapStateToProps = (state) => {
-  return { user: state.user };
-};
+const HomePage = () => (
+  <Jumbotron>
+    <h1 className="display-5">Познакомьтесь с Adaptation — вашим умным помощником</h1>
+    <p className="lead">
+      Мы заботимся о ваших HR-процессах, пока вы заботитесь о своих сотрудниках. Любая информация по
+      прохождению испытательного срока будет всегда под рукой, синхронизирована и доступна для
+      работы.
+    </p>
+  </Jumbotron>
+);
 
-class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.onBuy = this.onBuy.bind(this);
-  }
-
-  onBuy(e) {
-    e.preventDefault();
-    this.props.history.push('/donate');
-  }
-
-  render() {
-    return (
-      <div>
-        <Row>
-          <Col sm={{ size: 10, offset: 1 }}>
-            <div className="empty-for-main-page-left" />
-            <h1>Познакомьтесь с Adaptation - вашим умным помощником</h1>
-            <p>
-              Мы заботимся о ваших HR-процессах, пока вы заботитесь о своих сотрудниках. Любая
-              информация по прохождению испытательного срока будет всегда под рукой,
-              синхронизирована и доступна для работы.
-            </p>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col sm={{ size: 2, offset: 6 }}>
-            <div className="empty-for-main-page-right" />
-            {!this.props.user && (
-              <Form action="/api/login" method="POST">
-                <Button color="primary" type="submit">
-                  Попробовать бесплатно
-                </Button>
-              </Form>
-            )}
-          </Col>
-          <Col sm={{ size: 3 }} className="ml-5">
-            <div className="empty-for-main-page-right" />
-            <Button color="primary" outline onClick={this.onBuy}>
-              Приобрести сейчас
-            </Button>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-}
-
-export default connect(mapStateToProps)(HomePage);
+export default HomePage;
