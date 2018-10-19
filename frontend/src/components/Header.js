f1b657003a3d3eb394f4d8e5d981c8e23538372f
Vladimir Maximov <me@mvx.io>
2018-10-19 21:46:48 +0300

    ADAPT-21 collapsing header (#79)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import { Button, Container, Form, Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import {
+  Button,
+  Collapse,
+  Form,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Nav,
+  Container,
+  NavLink,
+  NavItem,
+} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import '!style-loader!css-loader!./Header.css';
@@ -10,107 +21,50 @@ const mapStateToProps = (state) => {
   };
 };
 
-class UserInfo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    if (this.props.user)
-      return (
-        <NavItem className="mr-3">
-          <NavLink className="text-white" tag={Link} to="#">
-            Привет, {this.props.user.firstName}!
-          </NavLink>
-        </NavItem>
-      );
-    else return null;
-  }
-}
-
-const ConnectedUserInfo = connect(mapStateToProps)(UserInfo);
-
-class LoginLogoutButton extends React.Component {
-  render() {
-    const hidden = this.props.user
-      ? false
-      : this.props.path === '/' || this.props.path === '/donate';
-    return (
-      !hidden && (
-        <NavItem className="mr-3">
-          <Form action={this.props.user ? '/api/logout' : '/api/login'} method="POST">
-            <Button className="text-white" type="submit">
-              {this.props.user ? 'Выйти' : 'Войти'}
-            </Button>
-          </Form>
-        </NavItem>
-      )
-    );
-  }
-}
-
-const ConnectedLoginLogoutButton = connect(mapStateToProps)(LoginLogoutButton);
-
-class NavButtons extends React.Component {
-  render() {
-    if (this.props.user)
-      return (
-        <Nav className="mr-auto" navbar>
-          <NavItem className="ml-3">
-            <Button
-              className="text-white borderless-button"
-              outline
-              tag={Link}
-              to="/list_employees"
-            >
-              Список сотрудников
-            </Button>
-          </NavItem>
-          <NavItem className="ml-3">
-            <Button className="text-white borderless-button" outline tag={Link} to="/add_employee">
-              Добавить сотрудника
-            </Button>
-          </NavItem>
-          {this.props.user.isAdmin && (
-            <NavItem className="ml-3">
-              <Button className="text-white borderless-button" outline tag={Link} to="/admin_page">
-                Админка
-              </Button>
-            </NavItem>
-          )}
-        </Nav>
-      );
-    else return null;
-  }
-}
-
-const ConnectedNavButtons = connect(mapStateToProps)(NavButtons);
-
 class Header extends React.Component {
+  state = {
+    collapsed: true,
+  };
+
+  navbarColor = ADAPT_TEST_MODE ? 'dark progress-bar-striped' : 'dark';
+  navbarBrandText = ADAPT_TEST_MODE ? 'Adapt_test' : 'Adaptation';
+
+  toggleNavbar = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
   render() {
-    let navbarColor = 'dark';
-    let navbarBrandText = 'Adaptation';
-
-    if (ADAPT_TEST_MODE) {
-      navbarColor = 'dark progress-bar-striped';
-      navbarBrandText = 'Adapt_test';
-    }
-
+    const { user } = this.props;
     return (
-      <Navbar color={navbarColor} dark expand="md" className="mb-3">
-        <Container>
-          <NavbarBrand className="ml-4 text-white" tag={Link} to="/">
-            {navbarBrandText}
-          </NavbarBrand>
-          <ConnectedNavButtons />
-          <Nav className="ml-auto" navbar>
-            <ConnectedUserInfo />
-            <ConnectedLoginLogoutButton path={this.props.location.pathname} />
-          </Nav>
-        </Container>
-      </Navbar>
+      <div className="header">
+        <Navbar color={this.navbarColor} dark expand="md">
+          <Container>
+            <NavbarBrand tag={Link} to="/">
+              {this.navbarBrandText}
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggleNavbar} />
+            <Collapse isOpen={!this.state.collapsed} navbar>
+              <Nav navbar className="mr-auto">
+                <NavLink href="/list_employees">Сотрудники</NavLink>
+                <NavLink href="/add_employee">Добавить</NavLink>
+                {user.isAdmin && <NavLink href="/admin_page">Админка</NavLink>}
+              </Nav>
+              <Nav>
+                <Form action="/api/logout" method="POST">
+                  <NavItem>
+                    <span className="user-info">Привет, {user.firstName}!</span>
+                    <Button type="submit">Выйти</Button>
+                  </NavItem>
+                </Form>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
     );
   }
 }
 
-export default Header;
+export default connect(mapStateToProps)(Header);
