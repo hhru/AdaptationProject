d07d6b506c67684104857ecf31b46bd0b625256d
Leonid Vinogradov <leonid.52669@gmail.com>
2018-11-23 22:09:54 +0300

    ADAPT-19 date format for docs

--- backend/src/main/java/ru/hh/school/adaptation/services/documents/TaskDocumentGenerator.java
+++ backend/src/main/java/ru/hh/school/adaptation/services/documents/TaskDocumentGenerator.java
@@ -1,5 +1,6 @@
 package ru.hh.school.adaptation.services.documents;
 
+import java.text.SimpleDateFormat;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.apache.poi.xwpf.usermodel.XWPFTableCell;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 public abstract class TaskDocumentGenerator extends DocumentGenerator {
+  private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
   public TaskDocumentGenerator(String templateName) {
     super(templateName);
@@ -47,8 +49,8 @@ public abstract class TaskDocumentGenerator extends DocumentGenerator {
     String chiefFio = CommonUtils.makeFioFromPersonalInfo(employee.getChief());
     String mentroFio = employee.getMentor() == null ? "" : CommonUtils.makeFioFromPersonalInfo(employee.getMentor());
     replacements.put("{{employee.fullName}}", fio);
-    replacements.put("{{employee.employmentDate}}", employee.getEmploymentDate().toString());
-    replacements.put("{{employee.endDate}}", employee.getFinalDate().toString());
+    replacements.put("{{employee.employmentDate}}", simpleDateFormat.format(employee.getEmploymentDate()));
+    replacements.put("{{employee.endDate}}", simpleDateFormat.format(employee.getFinalDate()));
     replacements.put("{{employee.position}}", employee.getPosition());
     replacements.put("{{employee.chief}}", chiefFio);
     replacements.put("{{employee.mentor}}", mentroFio);
