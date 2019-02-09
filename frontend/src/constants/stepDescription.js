ce3aec4ebcd748c2eb139fb98bddcc401d433d89
Vitaly Vasin <v.vasin@hh.ru>
2019-02-09 19:12:48 +0300

    ADAPT-65 fix employee list sorting

---
+++ frontend/src/constants/stepDescription.js
@@ -0,0 +1,47 @@
+export default {
+  ADD: {
+    order: 0,
+    fullTextToDisplay: 'Добавление в систему',
+    progressPercent: 5,
+  },
+  TASK_LIST: {
+    order: 1,
+    fullTextToDisplay: 'Постановка задач',
+    progressPercent: 10,
+  },
+  WELCOME_MEETING: {
+    order: 2,
+    fullTextToDisplay: 'Welcome встреча',
+    progressPercent: 20,
+  },
+  INTERIM_MEETING: {
+    order: 3,
+    fullTextToDisplay: 'Промежуточная встреча',
+    progressPercent: 40,
+  },
+  INTERIM_MEETING_RESULT: {
+    order: 4,
+    fullTextToDisplay: 'Итоги промежуточной встречи',
+    progressPercent: 50,
+  },
+  FINAL_MEETING: {
+    order: 5,
+    fullTextToDisplay: 'Финальная встреча',
+    progressPercent: 70,
+  },
+  FINAL_MEETING_RESULT: {
+    order: 6,
+    fullTextToDisplay: 'Итоги финальной встречи',
+    progressPercent: 80,
+  },
+  QUESTIONNAIRE: {
+    order: 7,
+    fullTextToDisplay: 'Подведение итогов ИС',
+    progressPercent: 90,
+  },
+  NONE: {
+    order: 8,
+    fullTextToDisplay: 'ИС завершен',
+    progressPercent: 100,
+  },
+};
